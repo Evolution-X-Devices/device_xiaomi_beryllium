959d14e5ab3af4800f2d6f7705fc6fa1240ce496
Le Hong Duc <hongducthbk123@gmail.com>
2025-03-27 16:58:09 +0700

    beryllium: Implement keyhandler for fingerprint shutter
     * prevents fp from triggering other events such as
       vibrate on long press
    
    Fixes: 6b76830fbd214ceabb04445cbbcb3bf849897bec ("keylayout: Redirect keycode 96 to CAMERA")
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Ic7fec6d37d42b3d5fa8ff78935eaa85e212d64f5

--- device.mk
+++ device.mk
@@ -84,6 +84,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
+PRODUCT_PACKAGES += \
+    KeyHandler
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
