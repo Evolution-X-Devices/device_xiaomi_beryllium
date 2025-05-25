b967dac0941cd1637cb607a9b08d72b5065455b2
Le Hong Duc <hongducthbk123@gmail.com>
2025-05-25 20:50:56 +0700

    beryllium: Disable blur

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -9,6 +9,8 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_ENABLE_BLUR := false
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_beryllium
 PRODUCT_DEVICE := beryllium
